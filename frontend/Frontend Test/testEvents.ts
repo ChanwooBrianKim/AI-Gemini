1b33b44ee7133fd893f518ceb7bf2dc5113c3d55
ChanwooBrianKim <brian980729@gmail.com>
2024-08-28 14:27:09 +1000

    test

---
+++ frontend/Frontend Test/testEvents.ts
@@ -0,0 +1,7 @@
+    // // Delete all chats from local storage when button is clicked
+    // deleteChatButton.addEventListener("click", () => {
+    //     if (confirm("Are you sure you want to delete all chats?")) {
+    //         localStorage.removeItem("savedChats");
+    //         loadLocalStorageData(chatList, toggleThemeButton);
+    //     }
+    // });
